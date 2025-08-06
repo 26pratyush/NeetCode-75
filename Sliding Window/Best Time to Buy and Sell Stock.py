7a5b76bf889ac9e47e2ab9a66f46d1411593d225
Pratyush <66518297+26pratyush@users.noreply.github.com>
2025-08-06 12:23:16 +0530

    Create Best Time to Buy and Sell Stock.py

---
+++ Sliding Window/Best Time to Buy and Sell Stock.py
@@ -0,0 +1,27 @@
+#You are given an integer array prices where prices[i] is the price of NeetCoin on the ith day.
+#You may choose a single day to buy one NeetCoin and choose a different day in the future to sell it.
+#Return the maximum profit you can achieve. You may choose to not make any transactions, in which case the profit would be 0.
+
+#Example 1: Input: prices = [10,1,5,6,7,1]
+#Output: 6: Explanation: Buy prices[1] and sell prices[4], profit = 7 - 1 = 6.
+
+#Brute Force: O(n²)
+profit=0
+for i in range(len(prices)):
+    price=prices[i]
+    for j in range(0,i):        #not i-1
+        if prices[j]<prices[i] and prices[i]-prices[j]>profit:  #to check profit before rewriting
+            profit=prices[i]-prices[j]
+return profit
+
+#Sliding Window: O(n)
+minprice=float('inf')
+maxprofit=0
+
+for price in prices:
+    if price<minprice:
+        minprice=price
+    if price-minprice>maxprofit:
+        maxprofit=price-minprice
+
+return maxprofit
