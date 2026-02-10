970e2d1841b049fae2c5778fc3f3cf4cc8d29c40
Pratyush <66518297+26pratyush@users.noreply.github.com>
2026-02-10 12:01:39 +0530

    Update Valid Anagram.py

--- Arrays & Hashing/Valid Anagram.py
+++ Arrays & Hashing/Valid Anagram.py
@@ -3,24 +3,31 @@
 
 #Example 1: Input: s = "racecar", t = "carrace"
 #Output: true
-
 #Example 2: Input: s = "jar", t = "jam"
 #Output: false
 
+#Method 1: Sort strings and compare:
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s)!=len(t):
             return False
         else:
-            CountS, CountT={}, {}
+            s=sorted(s)
+            t=sorted(t)
+            return s==t
 
+#Method 2: HashMap
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        
+        else:
+            countS, countT={},{}
             for i in range(len(s)):
-                CountS[s[i]]=1+CountS.get(s[i],0)
-                CountT[t[i]]=1+CountT.get(t[i],0)
-            if CountS==CountT:
-                return True
-            else:
-                return False    
+                countS[s[i]]=countS.get(s[i], 0) + 1
+                countT[t[i]]=countT.get(t[i], 0) + 1
+            return countS==countT
 
 #A hash map (also called a dictionary in Python) is a data structure that stores key-value pairs 
 #and allows for fast access, insertion, and updates.
