57fbd6dba272a538fe7f64fe5f3acc287f5c6eca
Pratyush <66518297+26pratyush@users.noreply.github.com>
2026-02-12 22:28:33 +0530

    Update Group Anagrams.py

--- Arrays & Hashing/Group Anagrams.py
+++ Arrays & Hashing/Group Anagrams.py
@@ -5,17 +5,15 @@
 #Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
 
 #Solution:
+from typing import List
+from collections import defaultdict
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        from collections import defaultdict, Counter
-
         anagram_map = defaultdict(list)
+        
         for word in strs:
-            count = Counter(word)                    # Count character frequencies
-            key = tuple(sorted(count.items()))       # Convert to a hashable key
-            anagram_map[key].append(word)
-        return list(anagram_map.values())
+            sorted_word = ''.join(sorted(word))    # Sort the word to get the key
+            anagram_map[sorted_word].append(word)  # Append the original word to the list for this key
 
-# This solution groups anagrams by counting each word’s character frequencies using Counter.
-# It converts the frequency dictionary into a sorted tuple (key) so it can be used as a dictionary key (as tuple is immutable).
-# Words with the same character counts share the same key and are grouped together in anagram_map.
+        return list(anagram_map.values())          # Return all grouped anagram lists
