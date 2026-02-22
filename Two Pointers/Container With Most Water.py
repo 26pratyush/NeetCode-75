5822add435df50cd7499a73ebc1cdd8ebca755bc
Pratyush <66518297+26pratyush@users.noreply.github.com>
2026-02-22 21:10:51 +0530

    Update Container With Most Water.py

--- Two Pointers/Container With Most Water.py
+++ Two Pointers/Container With Most Water.py
@@ -4,30 +4,21 @@
 #Example 1: Input: height = [1,7,2,5,4,7,3,6]
 #Output: 36  ( 7 and 6, actual height = smaller ele -> 6. Then width = 7's indice - 6's indice = 7 - 1 = 6. Area = 6 * 6.
 
-#Brute Force: O(n)^2
-res=0
-for i in range(len(heights)-1):
-    for j in range(i+1, len(heights)):
-        height=min(heights[i],heights[j])
-        width=j-i
-        amt=height*width
-        if amt>res:
-            res=amt
-return res
-
 #Two pointers: O(n)
-res=0
-i,j=0,len(heights)-1
-
-while(j>i):
-    width=j-i
-    height=min(heights[i],heights[j])
-    area=height*width
-    if area>res:
-        res=area
-    if heights[i]<heights[j]:
-        i+=1
-    else:
-        j-=1
-    
-return res
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        left,right=0,len(heights)-1
+        maxw=0
+        area=0
+        while left<right:
+            area=(right-left)*min(heights[left],heights[right])
+            if area>maxw:
+                maxw=area
+            if heights[left]>heights[right]:
+                right-=1
+            elif heights[left]<heights[right]:
+                left+=1
+            elif heights[left]==heights[right]:
+                left+=1
+                right-=1
+        return maxw
