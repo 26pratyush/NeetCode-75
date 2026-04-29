3f257b63ab1a43094e06605a53cdb089bc3ebfee
Pratyush <66518297+26pratyush@users.noreply.github.com>
2026-04-29 15:09:53 +0530

    Update Container With Most Water.py

--- Two Pointers/Container With Most Water.py
+++ Two Pointers/Container With Most Water.py
@@ -8,12 +8,12 @@
 class Solution:
     def maxArea(self, heights: List[int]) -> int:
         left,right=0,len(heights)-1
-        maxw=0
-        area=0
+        maxw, area = 0,0
+        
         while left<right:
             area=(right-left)*min(heights[left],heights[right])
-            if area>maxw:
-                maxw=area
+            maxw=max(area, maxw)
+            
             if heights[left]>heights[right]:
                 right-=1
             elif heights[left]<heights[right]:
